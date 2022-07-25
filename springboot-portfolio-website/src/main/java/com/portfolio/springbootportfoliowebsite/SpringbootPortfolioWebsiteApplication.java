e6198ff521b58485f81c24e126a32d9f18f54826
Amrit Shrivastava <amritshrivastava1@gmail.com>
2022-07-25 19:36:15 +0530

    initial commit- starting the project

---
+++ springboot-portfolio-website/src/main/java/com/portfolio/springbootportfoliowebsite/SpringbootPortfolioWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.springbootportfoliowebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPortfolioWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPortfolioWebsiteApplication.class, args);
+	}
+
+}
---
+++ springboot-portfolio-website/src/test/java/com/portfolio/springbootportfoliowebsite/SpringbootPortfolioWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.springbootportfoliowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPortfolioWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
